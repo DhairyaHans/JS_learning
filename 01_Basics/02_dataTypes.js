f56c9d8d9dafaee971817177998961d13276acd7
Dhairya Hans <dhairyahans2001@gmail.com>
2024-05-05 23:02:00 +0530

    Datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // To Treat all the JS code as per newer Js Standard
+
+// alert(3+3)      // We are using NodeJs, not browser, thats why It will not run as it is
+
+let name = "Dhairya";
+let age = 18;
+let isLoggedIn = true;
+let state; // Undefined
+
+// number => 2**53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (absence of a value)
+// undefined => undefined
+// symbol => unique (to identify unique values)
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
