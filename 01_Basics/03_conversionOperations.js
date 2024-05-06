77a74239a40a97934f81abfb7d5c2bf65c701c09
Dhairya Hans <dhairyahans2001@gmail.com>
2024-05-07 03:54:46 +0530

    Operations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -28,8 +28,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someData = 33;
 
 let stringData = String(someData);
-console.log(stringData);
-console.log(typeof stringData);
+// console.log(stringData);
+// console.log(typeof stringData);
 
 // 33 => "33"
 // true => "true"
+
+// ****************** OPERATIONS *********************
+
+let value = 3;
+let negVal = -value;
+
+// console.log(negVal);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 2);
+
+let str1 = "Hello";
+let str2 = " Dhairya";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);                // 12 (string)
+// console.log(1 + "2");                // 12
+// console.log("1" + 2 + 2);            // 122
+// console.log(1 + 2 + "2");            // 32
+// console.log(1 + 2 + "3" + 4 + 5);    // 3345
+// console.log(2 * 5 + "1");            // 101
+
+// Performs mathmatical operations till first string, then concatenates
+// all the values
+
+// console.log(+true); // 1 (type conversion to Int)
+// console.log(+""); // 0
